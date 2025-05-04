122031118c499ba2b9010da5c291eb0879b63840
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-05-04 08:23:26 -0400

    initialized library array and created book constructor

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // JS for Odin Library
 
+const library = []; 
+
+function Book(title, author, length, status) {
+    this.title = title;
+    this.author = author;
+    this.length = length;
+    this.status = status;
+    this.id = crypto.randomUUID();
+}
