222af3443682b07e3e7b9ff56ba2f833d3cd46df
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-05-04 09:32:58 -0400

    Added function that displays library books on page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const library = []; 
 
+// Object constructor for creating new book
 function Book(title, author, length, status) {
     this.title = title;
     this.author = author;
@@ -10,7 +11,63 @@ function Book(title, author, length, status) {
     this.id = crypto.randomUUID();
 }
 
+// Creates new book and adds it to library 
 function addBookToLibrary(title, author, length, status) {
     const book = new Book (title, author, length, status);
     library.push(book);
+}
+
+// Loops through library and displays each book on page
+function displayLibrary() {
+    for (const book of library) {
+        const bookList = document.querySelector("#book-list")
+        
+        const newBook = document.createElement("div");
+        newBook.classList.add("book");
+        newBook.dataset.bookID = book.id;
+
+        const bookTitle = document.createElement("div");
+        bookTitle.classList.add("book-title");
+        bookTitle.textContent = book.title;
+        newBook.appendChild(bookTitle);
+
+        const bookBody = document.createElement("div");
+        bookBody.classList.add("book-body");
+        newBook.appendChild(bookBody);
+
+        const bookContent = document.createElement("div");
+        bookContent.classList.add("book-content");
+        bookBody.appendChild(bookContent);
+
+        const bookAuthor = document.createElement("div");
+        bookAuthor.classList.add("book-content-item");
+        bookAuthor.textContent = book.author;
+        bookContent.appendChild(bookAuthor);
+
+        const bookLength = document.createElement("div");
+        bookLength.classList.add("book-content-item");
+        bookLength.textContent = book.length;
+        bookContent.appendChild(bookLength);
+
+        const bookStatus = document.createElement("div");
+        bookStatus.classList.add("book-content-item");
+        bookStatus.textContent = book.status;
+        bookContent.appendChild(bookStatus);
+
+        const bookButtonRow = document.createElement("div");
+        bookButtonRow.classList.add("book-button-row");
+        bookBody.appendChild(bookButtonRow);
+
+        const changeStatusButton = document.createElement("button");
+        changeStatusButton.classList.add("book-button");
+        changeStatusButton.textContent = "Change status";
+        bookButtonRow.appendChild(changeStatusButton);
+
+        const removeBookButton = document.createElement("button");
+        removeBookButton.classList.add("book-button");
+        removeBookButton.textContent = "Remove book";
+        bookButtonRow.appendChild(removeBookButton);
+
+        bookList.appendChild(newBook);
+    }
 }
