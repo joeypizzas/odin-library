eb2a1b1feb7192d78719c5052d55fb2b945d2c12
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-07-06 09:26:39 -0400

    refactored constructor to use class

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 const library = []; 
 
-function Book(title, author, length, status) {
-    this.title = title;
-    this.author = author;
-    this.length = length;
-    this.status = status;
-    this.id = crypto.randomUUID();
+class Book {
+    constructor(title, author, length, status) {
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.status = status;
+        this.id = crypto.randomUUID();
+    }
 }
 
 function addBookToLibrary(title, author, length, status) {
